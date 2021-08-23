9b58b2bf1d15530f7cb1d0217609bdb42fb00ed8
joselopez <jose.lopez@payco.co>
2021-08-23 18:37:27 -0400

    feat: ruta principal, ajustes en los require, carpta script con tabla paises

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+require("./src/view/inicio/index.php");
--- src/view/inicio/index.php
+++ src/view/inicio/index.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Web</title>
-    <link rel="stylesheet" href="../../asssets/style.css" />
+    <link rel="stylesheet" href="./src/asssets/style.css" />
+    <link rel="stylesheet" href="../../asssets/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
           crossorigin="anonymous" />
 </head>
 <body>
-<?php require('../header-1.php'); ?>
+<?php require_once __DIR__."/../header-1.php"; ?>
 <!-- content -->
 <div class="mt-1">
-    <?php require('./content.php'); ?>
+    <?php require_once __DIR__."/content.php"; ?>
 </div>
-
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </html>
